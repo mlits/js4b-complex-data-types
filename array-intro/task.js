66fe50b7326fd51d30c4edc7602e5cc942b4c712
Kyrylo Gerasymenko <kyrylo.gerasymenko@globallogic.com>
2018-12-05 16:44:39 +0200

    initial commit

---
+++ array-intro/task.js
@@ -0,0 +1,26 @@
+// sort pair of numbers in descending order.
+// pair is always an array of just 2 numbers.
+function sortPairDesc(pair) {
+  return pair;
+}
+
+// arr1 and arr2 are arrays.
+// return longer array or first one.
+// if sizes are equal - return arr1
+function getLonger(arr1, arr2) {
+  return arr2;
+}
+
+// values is an array with numbers.
+// return sum of all the elements.
+function getSum(values) {
+  return 0;
+}
+
+// * arr1 and arr2 are arrays with numbers.
+// return array where sum of all the numbers is greater.
+// if sum is equal - return longer array
+function getWithBiggerSum(arr1, arr2) {
+  return arr1;
+}
+
