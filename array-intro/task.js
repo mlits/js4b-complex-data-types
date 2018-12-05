8122f2d6109e473a264337502fcdd73b79889c21
BIlous Alexander <d818651@gmail.com>
2018-12-05 23:59:06 +0200

    second solution

--- array-intro/task.js
+++ array-intro/task.js
@@ -1,8 +1,8 @@
 // sort pair of numbers in descending order.
 // pair is always an array of just 2 numbers.
 function sortPairDesc(pair) {
-  return pair.sort(function (prev, current) {
-    return current - prev;
+  return pair.sort(function (a, b) {
+    return b - a;
   });
 }
 
@@ -17,21 +17,19 @@ function getLonger(arr1, arr2) {
 // values is an array with numbers.
 // return sum of all the elements.
 function getSum(values) {
-  return values.reduce(function (a, b) {
-    return a + b;
-  });
+  return values.reduce(sum);
 }
 
 // * arr1 and arr2 are arrays with numbers.
 // return array where sum of all the numbers is greater.
 // if sum is equal - return longer array
 function getWithBiggerSum(arr1, arr2) {
-  function getSum(a, b) {                         // можно было вынести данную функцию в глабольную и сократить общий код
-    return a + b;
-  }
-  if (arr1.reduce(getSum) == arr2.reduce(getSum)) return getLonger(arr1, arr2);
-  if (arr1.reduce(getSum) > arr2.reduce(getSum))
+  if (arr1.reduce(sum) == arr2.reduce(sum)) return getLonger(arr1, arr2);
+  if (arr1.reduce(sum) > arr2.reduce(sum))
     return arr1; else
     return arr2;
 }
 
+function sum(a, b) {                         
+  return a + b;
+}
