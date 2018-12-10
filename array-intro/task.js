4c77d9560f57fdc3d7be2d8e0170732b1ed88cf3
Ivansk27 <Ivan2787.28@ukr.net>
2018-12-10 16:21:25 +0200

    ivans array

--- array-intro/task.js
+++ array-intro/task.js
@@ -1,6 +1,8 @@
 // sort pair of numbers in descending order.
 // pair is always an array of just 2 numbers.
 function sortPairDesc(pair) {
+  debugger;
+  pair.reverse();
   return pair;
 }
 
@@ -8,19 +10,32 @@ function sortPairDesc(pair) {
 // return longer array or first one.
 // if sizes are equal - return arr1
 function getLonger(arr1, arr2) {
-  return arr2;
+  if (arr1.length < arr2.length) {
+    return arr2
+  } else {
+    return arr1;
+  }
 }
 
 // values is an array with numbers.
 // return sum of all the elements.
 function getSum(values) {
-  return 0;
+  // // var sum = 0;
+  // // for (var a = 0; a < values.length; a++) {
+  // //   sum += values[a];
+  // }
+  var result = values.reduce(function (sum, current) {
+    return sum + current;
+  }, 0);
+  return result;
 }
 
 // * arr1 and arr2 are arrays with numbers.
 // return array where sum of all the numbers is greater.
 // if sum is equal - return longer array
 function getWithBiggerSum(arr1, arr2) {
+
   return arr1;
+
 }
 
