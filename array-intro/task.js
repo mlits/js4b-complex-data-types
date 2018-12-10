58dfbd108f039af0f8daf37c2ddad7746bb0afff
alexandr-semyonov <alexandrbublik@gmail.com>
2018-12-10 16:44:17 +0200

    complete array task v 1.0

--- array-intro/task.js
+++ array-intro/task.js
@@ -1,6 +1,12 @@
 // sort pair of numbers in descending order.
 // pair is always an array of just 2 numbers.
 function sortPairDesc(pair) {
+  var num1 = pair[0];
+  var num2 = pair[1];
+  if (num1 < num2){
+    pair[0] = num2;
+    pair[1] = num1;
+  }
   return pair;
 }
 
@@ -8,19 +14,33 @@ function sortPairDesc(pair) {
 // return longer array or first one.
 // if sizes are equal - return arr1
 function getLonger(arr1, arr2) {
-  return arr2;
+  if (arr1.length < arr2.length){
+    return arr2;
+  }
+  return arr1;
 }
 
 // values is an array with numbers.
 // return sum of all the elements.
 function getSum(values) {
-  return 0;
+  var sum = 0;
+  for (var i = 0; i < values.length; i++) {
+    sum += values[i];    
+  }
+  return sum;
 }
 
 // * arr1 and arr2 are arrays with numbers.
 // return array where sum of all the numbers is greater.
 // if sum is equal - return longer array
 function getWithBiggerSum(arr1, arr2) {
+  var sum1 = getSum(arr1);
+  var sum2 = getSum(arr2);
+  if (sum1 < sum2){
+    return arr2;
+  } else if (sum1 == sum2){
+    return getLonger(arr1, arr2);
+  }
   return arr1;
 }
 
