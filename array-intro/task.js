7a92420940509e6096a75fa75f79085f65ebcab0
gelbman20 <gelbman20@gmail.com>
2018-12-11 23:18:42 +0200

    SYS: complete

--- array-intro/task.js
+++ array-intro/task.js
@@ -3,21 +3,25 @@
 // pair is always an array of _just_ 2 numbers.
 // please _don't_ use Array.prototype.sort() method
 function sortPairDesc(pair) {
-  return pair;
+  return pair.sort(function (a, b) {
+    return a - b;
+  }).reverse();
 }
 
 // arr1 and arr2 are arrays.
 // return longer array or first one.
 // if sizes are equal - return arr1
 function getLonger(arr1, arr2) {
-  return arr2;
+  return arr1.length !== arr2.length && arr1.length < arr2.length ? arr2 : arr1
 }
 
 // values is an array with numbers.
 // return sum of all the elements.
 // please don't over-complicate it, use _for_ loop
 function getSum(values) {
-  return 0;
+  return values.reduce(function (a, b) {
+    return a + b;
+  })
 }
 
 // * arr1 and arr2 are arrays with numbers.
@@ -25,5 +29,12 @@ function getSum(values) {
 // if sum is equal - return longer array
 // note: you can use functions implemented above
 function getWithBiggerSum(arr1, arr2) {
-  return arr1;
+  var arr1Length = arr1.length;
+  var arr2Length = arr2.length;
+  var arr1Sum = getSum(arr1);
+  var arr2Sum = getSum(arr2);
+
+  if (arr1Sum === arr2Sum) return getLonger(arr1, arr2);
+
+  return arr1Sum !== arr2Sum && arr1Sum < arr2Sum ? arr2 : arr1
 }
--- array-methods/task.js
+++ array-methods/task.js
@@ -2,13 +2,13 @@
 // false otherwise
 function isVowel(letter) {
   var vowels = ['a', 'o', 'u', 'y', 'i', 'e'];
-  return;
+  return vowels.indexOf(letter.toLowerCase()) >= 0 ? true : false;
 }
 
 // 'hello' => 'olleh'
 // hint: you may want to use .split(), .reverse() and one more function ;)
 function reverse(str) {
-  return '';
+  return str.split('').reverse().join('');
 }
 
 // luckyNumbers is an array of numbers which besides the lucky numbers
@@ -16,7 +16,9 @@ function reverse(str) {
 // requirements: use .indexOf(), slice() and .splice(),
 // _DO NOT_ modify the array passed as the parameter
 function removeUnlucky(luckyNumbers) {
-  return [];
+  var array = luckyNumbers.slice();
+  array.splice(array.indexOf(13), 1);
+  return array;
 }
 
 
@@ -25,5 +27,19 @@ function removeUnlucky(luckyNumbers) {
 // check tests to understand what that means. and false otherwise
 // hint: organize a stack, using .push() and .pop() methods
 function isBalanced(str) {
-  return;
+  var array = str.split('');
+  var newArray = []
+  var newString;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === '(' || array[i] === ')' || array[i] === '[' || array[i] === ']') {
+      newArray.push(array[i]);
+    }
+  }
+
+  newString = newArray.join('');
+
+  if (newString === '()' || newString === '[]' || newString === '([])' || newString === '[()]') return true
+  return false
 }
+
