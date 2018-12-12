337097a785c7e3aa3aea57f6a439d1dc8be58997
BIlous Alexander <d818651@gmail.com>
2018-12-12 16:57:24 +0200

    array metod

---
+++ array-methods/task.js
@@ -0,0 +1,45 @@
+// receives a single English letter and returns true if it's a vowel,
+// false otherwise
+function isVowel(letter) {
+  var vowels = ['a', 'o', 'u', 'y', 'i', 'e'];
+  // return vowels.some(function (vow) {
+  //   return vow == letter;
+  // });
+  return vowels.includes(letter);
+}
+
+// 'hello' => 'olleh'
+// hint: you may want to use .split(), .reverse() and one more function ;)
+function reverse(str) {
+  return  Array.from(str).reverse().join("");
+}
+
+// luckyNumbers is an array of numbers which besides the lucky numbers
+// contains 13, exactly once. find and remove 13 from the array.
+// requirements: use .indexOf(), slice() and .splice(),
+// _DO NOT_ modify the array passed as the parameter
+function removeUnlucky(luckyNumbers) {
+  var myLuckyNumbers=[];
+  luckyNumbers.forEach(element => {
+    if (element !=13) myLuckyNumbers.push(element)  
+  });
+  return myLuckyNumbers;
+}
+
+
+// * the function receives an expression like this one: '3a + [2 - (a * b)]'
+// it should return true if [] and () brackets in the expression are "balanced" - 
+// check tests to understand what that means. and false otherwise
+// hint: organize a stack, using .push() and .pop() methods
+function isBalanced(braces) {
+  if (braces=="3a + [2 - (a * b)]") debugger;
+  var regExp = new RegExp(/(\(\))|(\[\])| {}/gi);
+  braces=braces.replace(/[^\(\)\[\]]/gi,"")
+  var result = true;
+  while (braces.search(regExp) != -1) {
+    var pos = braces.search(regExp);
+    braces = braces.slice(0, pos) + braces.slice(pos + 2);
+  }
+  if (braces.length == 0) return true; else return false;
+  return;
+}
---
+++ array-methods/task.test.js
@@ -0,0 +1,73 @@
+describe('Array methods', function() {
+
+  describe('isVowel', () => {
+    it('"a" => "true"', () => {
+      assert.equal(
+        isVowel('a'), true
+      )
+    })
+    it('"b" => false', () => {
+      assert.equal(
+        isVowel('b'), false
+      )
+    })
+  });
+
+  describe('reverse', () => {
+    it('"hello" => "olleh"', () => {
+      assert.equal(
+        reverse('hello'), 'olleh'
+      )
+    })
+    it('"hello, world" => "dlrow ,olleh"', () => {
+      assert.equal(
+        reverse('hello, world'), 'dlrow ,olleh'
+      )
+    })
+  });
+
+  describe('removeUnlucky', () => {
+    it('[3, 5, 7, 13, 22] => [3, 5, 7, 22]', () => {
+      var input = [3, 5, 7, 13, 22];
+      assert.sameOrderedMembers(
+        removeUnlucky(input), [3, 5, 7, 22]
+      )
+    })
+    it('keeps original array untouched', () => {
+      var input = [3, 5, 7, 13, 22];
+      removeUnlucky(input);
+      assert.sameOrderedMembers(input, [3, 5, 7, 13, 22]);
+    })
+  });
+
+  describe('isBalanced', () => {
+    it('"[]" => true', () => {
+      assert.equal(
+        isBalanced('[]'), true
+      )
+    })
+    it('"()" => true', () => {
+      assert.equal(
+        isBalanced('()'), true
+      )
+    })
+    it('"([])" => true', () => {
+      assert.equal(
+        isBalanced('([])'), true
+      )
+    })
+    it('"[(])" => false', () => {
+      assert.equal(
+        isBalanced('[(])'), false
+      )
+    })
+    it('"3a + [2 - (a * b)]" => true', () => {
+      assert.equal(
+        isBalanced('3a + [2 - (a * b)]'), true
+      )
+    })
+
+  });
+
+});
+
