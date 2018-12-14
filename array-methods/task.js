cfab8870376372710d9e2b24b9b3f2a1183f2f32
BIlous Alexander <d818651@gmail.com>
2018-12-14 12:45:48 +0200

    array metod

--- array-methods/task.js
+++ array-methods/task.js
@@ -1,22 +1,30 @@
 // receives a single English letter and returns true if it's a vowel,
 // false otherwise
 function isVowel(letter) {
-  // var vowels = ['a', 'o', 'u', 'y', 'i', 'e'];
-  return;
+  var vowels = ['a', 'o', 'u', 'y', 'i', 'e'];
+  // return vowels.some(function (vow) {
+  //   return vow == letter;
+  // });
+  return vowels.includes(letter.toLowerCase());
 }
 
 // 'hello' => 'olleh'
 // hint: you may want to use .split(), .reverse() and one more function ;)
 function reverse(str) {
-  return '';
+  return Array.from(str).reverse().join("");
 }
+
 
 // luckyNumbers is an array of numbers which besides the lucky numbers
 // contains 13, exactly once. find and remove 13 from the array.
 // requirements: use .indexOf(), slice() and .splice(),
 // _DO NOT_ modify the array passed as the parameter
 function removeUnlucky(luckyNumbers) {
-  return [];
+  var myLuckyNumbers = [];
+  luckyNumbers.forEach(element => {
+    if (element != 13) myLuckyNumbers.push(element)
+  });
+  return myLuckyNumbers;
 }
 
 
@@ -24,6 +32,13 @@ function removeUnlucky(luckyNumbers) {
 // it should return true if [] and () brackets in the expression are "balanced" - 
 // check tests to understand what that means. and false otherwise
 // hint: organize a stack, using .push() and .pop() methods
-function isBalanced(str) {
-  return;
+function isBalanced(braces) {
+  var regExp = new RegExp(/(\(\))|(\[\])| {}/gi);
+  braces = braces.replace(/[^\(\)\[\]]/gi, "")
+  var result = true;
+  while (braces.search(regExp) != -1) {
+    var pos = braces.search(regExp);
+    braces = braces.slice(0, pos) + braces.slice(pos + 2);
+  }
+  if (braces.length == 0) return true; else return false;
 }
